4b7d2c75b7db478e036a56e86a89248d3d3dd3ba
Muhammad Pearl Ocshada <erilleo23@gmail.com>
2025-02-27 10:47:08 +0700

    Percobaan3

---
+++ ASD_Jobsheet3/Praktikum03/mataKuliah.java
@@ -0,0 +1,16 @@
+package Praktikum_ASD.ALSD_Jobsheet3.Praktikum03;
+
+public class mataKuliah {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jmlJam;
+
+    public mataKuliah(String kode, String nama, int sks, int jmlJam){
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jmlJam = jmlJam;
+    }
+    
+}
