c689d457eaba336f21aec172183576237c3fbc5b
Muhammad Pearl Ocshada <erilleo23@gmail.com>
2025-02-27 09:44:50 +0700

    Percobaan2Pertanyaan1
    Tambahkan method cetakInfo() pada class mahasiswa

--- ASD_Jobsheet3/Praktikum03/mahasiswa.java
+++ ASD_Jobsheet3/Praktikum03/mahasiswa.java
@@ -4,4 +4,11 @@ public class mahasiswa {
     public String nama;
     public String kelas;
     public float ipk;
+    public void cetakInfo(){
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+        System.out.println("----------------------------------------");
+    }
 }
